9365ed7e6cb916ccfbc208ffe65875b6418738b6
Renske <vluuks@gmail.com>
2018-07-25 11:06:20 +0200

    cleanup and toc now does dashes instead of spaces

--- Week 3/Temp.md
+++
@@ -1,6 +0,0 @@
-- basis van adapter uitleggen?
-- met plain java variant?
-- koppeling van data naar het returnen van een layout element
-
-- alle methods in de adapter class uitleggen
-- constructors van de verschillende adapters uitleggen? 
--- tocGenerator.py
+++ tocGenerator.py
@@ -3,12 +3,12 @@ with open('Content.md', 'r') as inf, open('ToC.txt', 'w') as outf:
 
         if(line[:3] == "## " and line[3:].rstrip() != "Table of contents"):
             title = line[3:].rstrip()
-            l = "- [" + title + "](#" + title.lower() + ")\n" 
+            l = "- [" + title + "](#" + title.replace(' ', '-').lower() + ")\n" 
             outf.write(l)
 
         elif(line[:3] == "###"):
             title = line[4:].rstrip()
-            l = "\t * [" + title + "](#" + title.lower() + ")\n"
+            l = "\t * [" + title + "](#" + title.replace(' ', '-').lower() + ")\n"
             outf.write(l)
 
     outf.close()
