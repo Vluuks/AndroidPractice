0c73bcac594b0e423cada9970a48ad2e32e461e7
Renske <vluuks@gmail.com>
2018-08-27 13:35:17 +0200

    access cleanup

---
+++ Week2/Lab3/AccessTest.java
@@ -0,0 +1,10 @@
+class AccessTest {
+    
+    // The main method is the start of the program
+    public static void main(String[] args) {
+        
+        Student al = new Student("Ada Lovelace", "Computer Science", 61283, 180);
+        Student ik = new Student("Immanuel Kant", "Philosophy", 81148, 180);
+        Student ja = new Student("Jeanne d'Arc", "History", 90382, 40);
+    }
+}
