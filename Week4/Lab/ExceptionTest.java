c1fda031afe899c13f435305ed7ba2a89e0b994e
Renske <vluuks@gmail.com>
2018-11-13 14:11:39 +0100

    exercises start

--- Week4/Lab/ExceptionTest.java
+++ Week4/Lab/ExceptionTest.java
@@ -3,7 +3,7 @@ class ExceptionTest {
     // The main method is the start of the program
     public static void main(String[] args) {
         
-        int[] data = {10,20,30,40,50,60,70,80,90};
+        int[] data = {10, 20, 30, 40, 50, 60, 70, 80, 90};
         int i;
 
         try {
