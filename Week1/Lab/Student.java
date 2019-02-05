4b3c54ff4d8fcd2be902160d0babe74bc3843786
Renske <vluuks@gmail.com>
2019-02-05 11:57:44 +0100

    attempt to fix again

--- Week1/Lab/Student.java
+++ Week1/Lab/Student.java
@@ -2,30 +2,18 @@
     Simple model class depicting a student.
     Native App Studio - University of Amsterdam
 */
-public class StudentTest {
+public class Student {
 
+    // Properties of the class
+    public String name;
+    public String program;
+    public int studentNumber; 
+    // ... add a property
 
-    private class Student
-        // Properties of the class
-        public String name;
-        public String program;
-        public int studentNumber; 
-        // ... add a property
-
-        // Method of the class
-        public void printInfo() {
-            System.out.println("This student is called " + name);
-            System.out.println("they are studying " + program);
-            System.out.println("and their student number is " + studentNumber);
-        }
+    // Method of the class
+    public void printInfo() {
+        System.out.println("This student is called " + name);
+        System.out.println("they are studying " + program);
+        System.out.println("and their student number is " + studentNumber);
     }
-    
-    // The main method is the start of the program
-    public static void main(String[] args) {
-        
-        System.out.println("Running main!");
-        
-        // You can add your code here
-    }
-    
 }
--- Week1/Lab/StudentTest.java
+++ Week1/Lab/StudentTest.java
@@ -1,8 +1,24 @@
 /*
-    Class that contains the main method.
+    Simple model class depicting a student.
     Native App Studio - University of Amsterdam
 */
 public class StudentTest {
+
+
+    private class Student
+        // Properties of the class
+        public String name;
+        public String program;
+        public int studentNumber; 
+        // ... add a property
+
+        // Method of the class
+        public void printInfo() {
+            System.out.println("This student is called " + name);
+            System.out.println("they are studying " + program);
+            System.out.println("and their student number is " + studentNumber);
+        }
+    }
     
     // The main method is the start of the program
     public static void main(String[] args) {
@@ -11,4 +27,5 @@ public class StudentTest {
         
         // You can add your code here
     }
+    
 }
