9faa600702f5eb876ce748ceaf07b3c5b1515af5
Renske <vluuks@gmail.com>
2019-02-05 12:03:03 +0100

    attempt to fix again

--- Week1/Lab/StudentTest.java
+++ Week1/Lab/StudentTest.java
@@ -4,28 +4,29 @@
 */
 public class StudentTest {
 
-
-    private class Student
-        // Properties of the class
-        public String name;
-        public String program;
-        public int studentNumber; 
-        // ... add a property
-
-        // Method of the class
-        public void printInfo() {
-            System.out.println("This student is called " + name);
-            System.out.println("they are studying " + program);
-            System.out.println("and their student number is " + studentNumber);
-        }
-    }
     
     // The main method is the start of the program
     public static void main(String[] args) {
         
         System.out.println("Running main!");
-        
         // You can add your code here
     }
     
 }
+
+
+class Student {
+    // Properties of the class
+    public String name;
+    public String program;
+    public int studentNumber; 
+    // ... add a property
+
+    // Method of the class
+    public void printInfo() {
+        System.out.println("This student is called " + name);
+        System.out.println("they are studying " + program);
+        System.out.println("and their student number is " + studentNumber);
+    }
+}
+
