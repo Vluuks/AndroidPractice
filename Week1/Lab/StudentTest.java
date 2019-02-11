9af9f047759916380b8de14cbbd96af32549afab
Renske <vluuks@gmail.com>
2019-02-11 12:20:30 +0100

    reverted bug bandaid fix

--- Week1/Lab/StudentTest.java
+++ Week1/Lab/StudentTest.java
@@ -13,20 +13,3 @@ public class StudentTest {
     }
     
 }
-
-
-class Student {
-    // Properties of the class
-    public String name;
-    public String program;
-    public int studentNumber; 
-    // ... add a property
-
-    // Method of the class
-    public void printInfo() {
-        System.out.println("This student is called " + name);
-        System.out.println("they are studying " + program);
-        System.out.println("and their student number is " + studentNumber);
-    }
-}
-
